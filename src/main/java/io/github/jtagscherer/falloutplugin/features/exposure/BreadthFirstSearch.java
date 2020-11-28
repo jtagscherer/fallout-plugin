62b96742ecc15db88258e02455df61fe48e23d8d
Jan Tagscherer <jan.tagscherer@gmail.com>
2020-11-28 21:14:26 +0100

    Make glass panes block radiation

--- src/main/java/io/github/jtagscherer/falloutplugin/features/exposure/BreadthFirstSearch.java
+++ src/main/java/io/github/jtagscherer/falloutplugin/features/exposure/BreadthFirstSearch.java
@@ -90,7 +90,7 @@ class BreadthFirstSearch {
         }
 
         Material blockMaterial = this.world.getBlockAt(location).getType();
-        if (blockMaterial.isOccluding() || this.isGlassBlock(blockMaterial) || blockMaterial == Material.FARMLAND) {
+        if (blockMaterial.isOccluding() || this.isGlassBlock(blockMaterial) || this.isGlassPane(blockMaterial) || blockMaterial == Material.FARMLAND) {
             return;
         }
 
@@ -107,6 +107,18 @@ class BreadthFirstSearch {
                 || blockMaterial == Material.BLUE_STAINED_GLASS || blockMaterial == Material.BROWN_STAINED_GLASS
                 || blockMaterial == Material.GREEN_STAINED_GLASS || blockMaterial == Material.RED_STAINED_GLASS
                 || blockMaterial == Material.BLACK_STAINED_GLASS;
+    }
+
+    private boolean isGlassPane(Material blockMaterial) {
+        return blockMaterial == Material.GLASS_PANE || blockMaterial == Material.WHITE_STAINED_GLASS_PANE
+                || blockMaterial == Material.ORANGE_STAINED_GLASS_PANE || blockMaterial == Material.MAGENTA_STAINED_GLASS_PANE
+                || blockMaterial == Material.LIGHT_BLUE_STAINED_GLASS_PANE || blockMaterial == Material.YELLOW_STAINED_GLASS_PANE
+                || blockMaterial == Material.LIME_STAINED_GLASS_PANE || blockMaterial == Material.PINK_STAINED_GLASS_PANE
+                || blockMaterial == Material.GRAY_STAINED_GLASS_PANE || blockMaterial == Material.LIGHT_GRAY_STAINED_GLASS_PANE
+                || blockMaterial == Material.CYAN_STAINED_GLASS_PANE || blockMaterial == Material.PURPLE_STAINED_GLASS_PANE
+                || blockMaterial == Material.BLUE_STAINED_GLASS_PANE || blockMaterial == Material.BROWN_STAINED_GLASS_PANE
+                || blockMaterial == Material.GREEN_STAINED_GLASS_PANE || blockMaterial == Material.RED_STAINED_GLASS_PANE
+                || blockMaterial == Material.BLACK_STAINED_GLASS_PANE;
     }
 
 }
